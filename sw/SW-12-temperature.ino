75e5c4fc324ab052e2dd90e35b1784726a54b00e
bsaxen <benny.saxen@gmail.com>
2016-04-10 15:29:36 +0200

    harmonization of 12

--- sw/SW-12-temperature.ino
+++ sw/SW-12-temperature.ino
@@ -1,9 +1,19 @@
 //==================================================
 // SW-12-temperature.ino
-// 2016-04-05
+// 2016-04-10
+//==================================================
+int app_id = 12;
+//==================================================
+// Configuration
+//==================================================
+int g_debug              = 0;
+const char* g_clientName = "SW-12";
+const char* g_confServer = "sercon.simuino.com";
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+int g_device_delay       = 20;
 //==================================================
 #define NFLOAT 2  // No of decimals i float value
-#define SIDN 4    // No of SIDs
+#define SIDN 2    // No of SIDs
 #define SID1 901  
 #define SID2 902  
 #define SID3 903  
@@ -12,13 +22,17 @@
 #define SID6 906
 #define SID7 907
 #define SID8 908
-int g_debug = 0;
-int app_id = 12;
+
+#define greenLed  13
+#define yellowLed 12
+#define redLed     5
 //==================================================
-#define MAX_SID 10
+#define MAX_SID 8
 #define MAX_ORDERS 100
+char g_server[120]; 
 int g_sids[10] = {SIDN,SID1,SID2,SID3,SID4,SID5,SID6,SID7,SID8};
-int g_device_delay = 10;
+char g_sIp[80];
+char g_rbuf[4000];
 
 // Arduino-RPi protocol
 #define NABTON_DATA     1 
@@ -32,15 +46,15 @@ int g_device_delay = 10;
 // D2
 // D3 Status Pin - Serial data received
 // D4 IR Data
-// D5 Status Pin - Serial data sent
+// D5 Red LED, Status Pin - Serial data sent
 // D6 DIR Stepper
 // D7 STEP Stepper
 // D8 SLEEP Stepper
 // D9 One Wire Data
 // D10  RX Bluetooth device
 // D11  TX Bluetooth device
-// D12 
-// D13
+// D12  Yellow LED
+// D13  Green LED
 //
 // A0
 // A1
@@ -71,14 +85,22 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress device[MAX_SID];
 int nsensors = 0;
+
+//=================================================
+void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
+//=================================================
+{
+asm volatile ("  jmp 0");  
+}   
+
 //==================================================
 // OLED I2C
 //==================================================
 
-//U8GLIB_SSD1306_128X64 u8g(13, 11, 10, 9);// SW SPI protocol(4 pins): SCK = 13, MOSI = 11, CS = 10, A0 = 9	
+//U8GLIB_SSD1306_128X64 u8g(13, 11, 10, 9);// SW SPI protocol(4 pins): SCK = 13, MOSI = 11, CS = 10, A0 = 9  
 //U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE); // Small display I2C protocol (2 pins)
 U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE); // Large display
-char dl[5][8],dm[5][8],dr[5][8];
+char dl[16][16],dm[16][16],dr[16][16];
 //=================================================
 void NB_oledDraw() 
 //=================================================
@@ -94,8 +116,8 @@ void draw()
 {
   // Horizontal pixels: 0 - 120
   // Vertical pixels: 0 - 63
-  //u8g.setFont(u8g_font_6x10);
-  u8g.setFont(u8g_font_unifont);
+  u8g.setFont(u8g_font_6x10);
+  //u8g.setFont(u8g_font_unifont);
   //u8g.setFont(u8g_font_osb21);
   
   u8g.drawStr( 0, 10, dl[1]);
@@ -104,15 +126,28 @@ void draw()
   u8g.drawStr( 0, 62, dl[4]);
 
   u8g.drawStr( 45, 10, dm[1]);
-  u8g.drawStr( 45, 27, dm[2]);
-  u8g.drawStr( 45, 45, dm[3]);
+  u8g.drawStr( 99, 27, dm[2]);
+  u8g.drawStr( 99, 45, dm[3]);
   u8g.drawStr( 45, 62, dm[4]);
 
-  u8g.drawStr( 90, 10, dr[1]);
-  u8g.drawStr( 90, 27, dr[2]);
-  u8g.drawStr( 90, 45, dr[3]);
-  u8g.drawStr( 90, 62, dr[4]);  
+  u8g.drawStr( 100, 10, dr[1]);
+  u8g.drawStr( 100, 27, dr[2]);
+  u8g.drawStr( 100, 45, dr[3]);
+  u8g.drawStr( 100, 62, dr[4]);  
 
+}
+//=================================================
+void blinkLed(int led,int number, int onTime)
+//================================================= 
+{
+  int i;
+  for(i=0;i<number;i++)
+  {
+    digitalWrite(led,HIGH);
+    delay(onTime);
+    digitalWrite(led,LOW);
+    delay(onTime);
+  }
 }
 //=================================================
 void NB_serialFlush()
@@ -130,7 +165,7 @@ int NB_sendToGwy(int mid, int sid, float data, int other)
   char msg1[100],msg2[50],checksum[20];
      strcpy(msg1," ");
      strcpy(msg2," ");
-     digitalWrite(5,HIGH);
+     digitalWrite(yellowLed,HIGH);
      // Mandatory part of message
      sprintf(msg1,"?sw=%d&mid=%d&nsid=%d&sid1=%d",app_id,mid,1,sid);
 if(g_debug==1){Serial.print("data:");Serial.println(data);}      
@@ -175,7 +210,7 @@ if(g_debug==1){Serial.print("part2:");Serial.println(part2);}
      
      // Send meassage
      Serial.println(msg1);
-     digitalWrite(5,LOW);
+     digitalWrite(yellowLed,LOW);
     return(other);
 }
 //=================================================
@@ -190,13 +225,13 @@ void recSerial()
   
   if (nx > 0) 
   {
-     digitalWrite(3, HIGH); 
+     digitalWrite(redLed,HIGH); 
      Serial.readBytes(nbbuff,nx);
      sscanf(nbbuff,"%d %d",&mid,&sid);
      sprintf(dr[3],"%d",nx);
      if(sid == SID1) // Check if control sid correct
      {
-       if(strstr(nbbuff,"NBC_DEVICE_DELAY") != NULL)
+       if(strstr(nbbuff,"DELAY") != NULL)
        {
           strcpy(dr[3],"DLY");
           sscanf(nbbuff,"%d %d %s %d",&mid,&sid,command,&g_device_delay);
@@ -205,7 +240,7 @@ void recSerial()
        strcpy(dr[4],"-");
        NB_oledDraw();
      }
-     digitalWrite(3, LOW); 
+     digitalWrite(redLed,LOW); 
   }
 }
 
@@ -216,9 +251,9 @@ void clearOled()
   int i;
   for(i=1;i<=4;i++)
   {
-    strcpy(dl[i],"-");
-    strcpy(dm[i],"-");
-    strcpy(dr[i],"-");
+    strcpy(dl[i]," ");
+    strcpy(dm[i]," ");
+    strcpy(dr[i]," ");
   }
 }
 
@@ -231,8 +266,14 @@ void setup()
   String str;
   Serial.begin(9600);
   NB_serialFlush();
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
+  
+  pinMode(greenLed,          OUTPUT);
+  pinMode(redLed,            OUTPUT);
+  pinMode(yellowLed,         OUTPUT);
+
+  digitalWrite(greenLed,  HIGH); //Device ON
+  digitalWrite(redLed,    HIGH); //No Server
+  digitalWrite(yellowLed, HIGH); //No Network
   // OLED
 //=================================================
 
@@ -251,40 +292,47 @@ void setup()
   clearOled();
 // One Wire
 //=================================================
-
+  digitalWrite(greenLed, HIGH); //Device ON
+  sprintf(dl[1],"%s",g_clientName);
+  sprintf(dr[2],"n");
+  sprintf(dr[3],"c");
+  sprintf(dr[4],"s");
   sensors.begin();
   nsensors = sensors.getDeviceCount();
-  if(nsensors > 0)
-  {
-    for(i=0;i<nsensors;i++)
-    {
-      sensors.getAddress(device[i], i);
-      sensors.setResolution(device[i], TEMPERATURE_PRECISION);
-    }
-  }
-
-  sensors.requestTemperatures();
-  for(i=1;i<=nsensors;i++)
-  {
-      tempC = sensors.getTempC(device[i-1]);    
-      str = String(tempC);
-      str.toCharArray(dl[i],8); 
-  }
-  g_sids[1] = SID1;
-  g_sids[2] = SID2;
-  g_sids[3] = SID3;
-  g_sids[4] = SID4;
-  g_sids[5] = SID5;
-  g_sids[6] = SID6;
-  g_sids[7] = SID7;
-  g_sids[8] = SID8;
-  
-  sprintf(dr[1],"%d",g_device_delay);
-  for(i=1;i<=SIDN;i++)
-  {
-    sprintf(dm[i],"%d",g_sids[i]);
-  }
+  sprintf(dr[1],"%2d",nsensors);
   NB_oledDraw();
+  
+//  if(nsensors > 0)
+//  {
+//    for(i=0;i<nsensors;i++)
+//    {
+//      sensors.getAddress(device[i], i);
+//      sensors.setResolution(device[i], TEMPERATURE_PRECISION);
+//    }
+//  }
+//
+//  sensors.requestTemperatures();
+//  for(i=1;i<=nsensors;i++)
+//  {
+//      tempC = sensors.getTempC(device[i-1]);    
+//      str = String(tempC);
+//      str.toCharArray(dl[i],8); 
+//  }
+//  g_sids[1] = SID1;
+//  g_sids[2] = SID2;
+//  g_sids[3] = SID3;
+//  g_sids[4] = SID4;
+//  g_sids[5] = SID5;
+//  g_sids[6] = SID6;
+//  g_sids[7] = SID7;
+//  g_sids[8] = SID8;
+//  
+//  sprintf(dr[1],"%d",g_device_delay);
+//  for(i=1;i<=SIDN;i++)
+//  {
+//    sprintf(dm[i],"%d",g_sids[i]);
+//  }
+//  NB_oledDraw();
 
 }
 //=================================================
@@ -294,18 +342,22 @@ void loop()
   int i;
   float tempC;
   String str;
-  
+
+    sprintf(dm[1],"%3d",g_device_delay);
+    sprintf(dl[3],"%s",g_server);
+    
     sensors.requestTemperatures();
     for(i=0;i<nsensors;i++)
     {
+      sprintf(dl[4],"%3d",g_sids[i+1]);
       tempC = sensors.getTempCByIndex(i);
-      dtostrf(tempC,5, NFLOAT, dl[i+1]);
+      dtostrf(tempC,5, NFLOAT, dm[4]);
       if(tempC != -127)i = NB_sendToGwy(NABTON_DATA,g_sids[i+1],tempC,i);
-      strcpy(dm[i+1],"*"); 
+      //strcpy(dm[i+1],"*"); 
       NB_oledDraw();
       delay(2000);  
       recSerial();
-      sprintf(dm[i+1],"%d",g_sids[i+1]);
+      sprintf(dl[4],"%d",g_sids[i+1]);
       NB_oledDraw();
     }
     delay(g_device_delay*1000);   
