0148212db46a586e0d2814124ccd02bc9dece3a7
bsaxen <benny.saxen@gmail.com>
2016-04-21 19:13:58 +0200

    Update SW-98-beacon.py
    Working version

--- sw/SW-98-beacon.py
+++ sw/SW-98-beacon.py
@@ -3,6 +3,7 @@
 # SW-98-beacon.py
 # 2016-04-21
 #==================================================
+import time
 import httplib
 import os
 #==================================================
@@ -14,9 +15,8 @@ g_server    = '78.67.160.17'
 g_sercon    = 'config.nabton.com'
 g_path      = '/sxndata/index.php'
 g_ipaddress = 'x.x.x.x'
-g_app_id    = 98
 g_delay     = 10
-g_name      = 'beacon_Kil'
+g_name      = 'uniqueName'
 #==================================================
 os.system("ifconfig > ipaddress.work")
 file = open('ipaddress.work','r')
@@ -26,25 +26,24 @@ for line in file:
         work=words[11].split(':')
         g_ipaddress = work[1] 
         print 'local ipaddress ' + g_ipaddress
-        
-while 1:
-    conn = httplib.HTTPConnection(g_server)
-    try:
-        req = g_path+ '?appid='+g_app_id+ '&ip=' + g_ipaddress + '&name=' + g_name
-        conn.request("GET", req)
-        try:
-            r1 = conn.getresponse()
-            if g_debug == 'YES':
-                print ("Server Response:-_- %s %s " % (r1.status, r1.reason))
-            data1 = r1.read()
-            if g_debug == 'YES':
-                print data1
-        except:
-            print '-_- No response from nb server'
-    except:
-        print '-_- Not able to connect to nb server'
-    conn.close()
-    time.sleep(g_delay)
-# End of file
 
-Enter file contents here
+req = g_path+ '?mid=5'+'&appid=98'+'&ip=' + g_ipaddress + '&name=' + g_name  
+     
+while 1:
+	conn = httplib.HTTPConnection(g_server)
+	try:
+		conn.request("GET", req)
+		try:
+			r1 = conn.getresponse()
+			if g_debug == 'YES':
+                		print ("Server Response:-_- %s %s " % (r1.status, r1.reason))
+            		data1 = r1.read()
+            		if g_debug == 'YES':
+                		print data1
+        	except:
+            		print '-_- No response from nb server'
+    	except:
+        	print '-_- Not able to connect to server '+g_server
+    	conn.close()
+    	time.sleep(g_delay)
+# End of file
