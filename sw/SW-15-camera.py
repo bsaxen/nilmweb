a6b84f7e9d238ba927d545de70b6821caa1352f2
bsaxen <benny.saxen@gmail.com>
2016-05-11 11:23:25 +0200

    Update SW-15-camera.py

--- sw/SW-15-camera.py
+++ sw/SW-15-camera.py
@@ -26,10 +26,37 @@ g_ipaddress = 'xx.xx.xx.xx'
 g_delay     = 10
 g_name      = 'SW-15-camera'
 g_scp       = 'folke@nilm.se:/var/www/html/sxn_photo/.'
-#def RCFS(sid,par):
-#	print par
-#	print sid
-#	return;
+#---------------------------------------------------
+def RCFS(sid,par):
+#---------------------------------------------------
+req = g_path+ "?appid=%d&mid=%d&name=%s&ip=%s&nsid=1&sid1=%d" % (g_swid,g_mid,g_name,g_ipaddress,g_sid)     
+while 1:
+	conn = httplib.HTTPConnection(g_server)
+	try:
+		conn.request("GET", req)
+		try:
+			r1 = conn.getresponse()
+			if g_debug == 'YES':
+                		print ("Server Response:-_- %s %s " % (r1.status, r1.reason))
+            		data1 = r1.read()
+            		# Check if FF_PHOTO
+                    	if "FF_PHOTO" in data1:
+                    		now = time.strftime("%Y-%m-%d-%H-%M-%S")
+				photo_name    = "SW-15-sid%d-%s.jpg" % (g_sid,now)
+				take_photo    = "raspistill -o %s -t 1000pi" % (photo_name)
+				scp_photo     = "scp %s %s" % (photo_name,g_scp)
+                    		os.system(take_photo);
+				os.system(scp_photo);
+            		if g_debug == 'YES':
+                		print data1
+        	except:
+            		print '-_- No response from server'
+    	except:
+        	print '-_- Not able to connect to server '+g_server
+    	conn.close()
+	print par
+	print sid
+	return;
 #==================================================
 # RCFS - Read configuration from server
 #==================================================
