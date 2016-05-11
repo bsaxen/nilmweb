44517866b8d6355f136d0c4f0e146f58bbb60742
bsaxen <benny.saxen@gmail.com>
2016-05-11 12:56:36 +0200

    Update config.php

--- sw/config.php
+++ sw/config.php
@@ -1,5 +1,32 @@
 <?php
+//=======================================
+function getConfigValue($sid,$par)
+//======================================= 
+{
+  $filename = $sid.'.conf';
+    $handle = fopen($filename, "r");
+    if ($handle) 
+    {    
+        while (($line = fgets($handle)) !== false) 
+        {
+          $line = trim($line);
+          
+        }
+        fclose($handle);
+    } 
+}
+
+//==============================================================================
+// Main program
+//==============================================================================
+
+
+if(isset($_GET['sid']) && isset($_GET['config']))
+{
+
+$sid    = $_GET['sid'];
 $config = $_GET['config'];
 
+}
 
 ?>
