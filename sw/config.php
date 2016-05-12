04ae9df3b4f188a4c0c31589f3ebacf873abbf0e
bsaxen <benny.saxen@gmail.com>
2016-05-12 13:43:11 +0200

    Update config.php

--- sw/config.php
+++ sw/config.php
@@ -39,7 +39,35 @@ function getConfigValue($sid,$par)
     }
     
 }
-
+//=======================================
+function listAllSids()
+//======================================= 
+{
+    system("ls *.conf > sidList.work");   
+}
+//=======================================
+function createSidConfig($sid)
+//======================================= 
+{
+    $filename = $sid.'.conf';
+    if (file_exists($filename)) 
+    {
+        echo("SID $sid configuration already exists");
+    }
+    else
+    {
+        $handle = fopen($filename, "w");
+        if ($handle) 
+        {
+            fwrite($handle, "RCFS_SERVER 78.67.160.17 \n");
+            fwrite($handle, "RCFS_PATH /sxndata/index.php \n");
+            fwrite($handle, "RCFS_DELAY 10 \n");
+            fwrite($handle, "RCFS_NAME NoName_$sid \n");
+            fwrite($handle, "RCFS_DIR_PHOTO folke@nilm.se:/var/www/html/sxn_photo \n");
+            fclose($handle);
+        }
+    }
+}
 //==============================================================================
 // Main program
 //==============================================================================
@@ -51,6 +79,30 @@ if(isset($_GET['sid']) && isset($_GET['config']))
     $config = $_GET['config'];
     $res = getConfigValue($sid,$config);
     echo("$sid $config $res");
+    die;
+}
+else
+{
+    $action = $_POST['f_action'];
+    if($action == 'createSidConfForm')
+    {
+        $sid = $_POST[f_sid];
+        createSidConfig($sid);
+    }
+    
+    echo("<h1>SID Configuration</h1>");
+    echo("<table border=\"1\">
+         <form action=\"config.php\" method=\"post\">
+            <input name=\"f_action\" type=\"hidden\" value=\"createSidConfForm\" />
+            
+            <tr><td>Data Name:</td><td> <input name=\"f_sid\" type=\"text\" size=\"20\" value=\"901\"/></td></tr>
+            
+            <tr><td><input name=\"f_submit\" type=\"submit\" value=\"Create SID Configuration\" /></td><td></td></tr>
+        </form></table>
+  ");
+
+    listAllSids();
+    
 }
 
 ?>
