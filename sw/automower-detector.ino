99e39ae9c7db053127edfc82eba726d19c32d1f9
bsaxen <benny.saxen@gmail.com>
2016-06-03 16:55:41 +0200

    Update automower-detector.ino

--- sw/automower-detector.ino
+++ sw/automower-detector.ino
@@ -1,8 +1,13 @@
+
 //==================================================
 // automower-detector.ino
 // 2016-06-03
 //==================================================
 #include <U8glib.h>
+
+int oneLed    = 13;
+int detectLed = 13;
+char appName[30] = {"AUTOMOWER"};
 //==================================================
 // OLED I2C
 //==================================================
@@ -16,7 +21,7 @@ void NB_oledDraw()
 //=================================================
 {
  u8g.firstPage();  
-  do {
+  do { pinMode(oneLed, OUTPUT);
         draw();
   } while( u8g.nextPage() ); 
 }
@@ -36,17 +41,18 @@ void draw()
   u8g.drawStr( 0, 62, dl[4]);
 
   u8g.drawStr( 45, 10, dm[1]);
-  u8g.drawStr( 99, 27, dm[2]);
-  u8g.drawStr( 99, 45, dm[3]);
+  u8g.drawStr( 45, 27, dm[2]);
+  u8g.drawStr( 45, 45, dm[3]);
   u8g.drawStr( 45, 62, dm[4]);
 
   u8g.drawStr( 100, 10, dr[1]);
   u8g.drawStr( 100, 27, dr[2]);
   u8g.drawStr( 100, 45, dr[3]);
   u8g.drawStr( 100, 62, dr[4]);  
+}
 //=================================================
 void clearOled()
-//================================================= 
+//================================== pinMode(oneLed, OUTPUT);=============== 
 {
   int i;
   for(i=1;i<=4;i++)
@@ -56,11 +62,45 @@ void clearOled()
     strcpy(dr[i]," ");
   }
 }
-}
-void setup() 
+//=================================================
+void detect()
+//================================================= 
 {
-
-  pinMode(13, OUTPUT);
+  int i,tmax=0,xmax = 30,nmax = 0,prev = 0,ifreq = 0;
+  int x,x1=0,x2=0,x3=0,ax;
+  float freq;
+  digitalWrite(detectLed, HIGH);
+  for(i=1;i<=10000;i++)
+  {
+    //digitalWrite(oneLed, LOW); 
+    int x = analogRead(A0);
+    ax = (x+x1+x2)/3;
+    if(tmax < ax)tmax = ax;
+    if(ax > xmax)
+    {
+      nmax++;
+      ifreq = ifreq + (i - prev);
+      prev = i;
+    }
+    x2 = x1;
+    x1 = x;
+    //Serial.println(x); 
+  }
+  freq = (float)ifreq/nmax;
+  sprintf(dm[2],"%d",xmax);
+  sprintf(dr[2],"%d",nmax);
+  sprintf(dr[4],"%d",(int)freq);
+  sprintf(dr[3],"%d",tmax);
+  digitalWrite(detectLed, LOW);
+}
+//=================================================
+void setup() 
+//=================================================
+{
+  Serial.begin(9600);
+  pinMode(oneLed, OUTPUT);
+  pinMode(detectLed, OUTPUT);
+  pinMode(12, OUTPUT);
 // OLED
 //=================================================
 
@@ -77,18 +117,17 @@ void setup()
     u8g.setHiColorByRGB(255,255,255);
   }
   clearOled();
-  digitalWrite(greenLed, HIGH); //Device ON
-  sprintf(dl[1],"%s",g_clientName);
-  sprintf(dr[2],"n");
-  sprintf(dr[3],"c");
-  sprintf(dr[4],"s");
-  sensors.begin();
+  //digitalWrite(oneLed, HIGH); //Device ON
+  sprintf(dl[1],"%s",appName);
+  sprintf(dl[2],"Signal");
+  sprintf(dl[3],"Code");
+  sprintf(dl[4],"Freq");
   NB_oledDraw();
+  tone(12,10000);
 }
 
-void loop() {
-  digitalWrite(13, HIGH);  
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);            
+void loop() 
+{
+  detect();
+  NB_oledDraw();                      
 }
