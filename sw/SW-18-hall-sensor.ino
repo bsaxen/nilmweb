1c6d08d47bb172b82abc3eca38c4f96675b267a4
bsaxen <benny.saxen@gmail.com>
2016-07-31 15:00:31 +0200

    Update SW-18-hall-sensor.ino

--- sw/SW-18-hall-sensor.ino
+++ sw/SW-18-hall-sensor.ino
@@ -1,11 +1,62 @@
-loat limit = 512;
+#include <U8glib.h>
 
+
+int limit = 512;
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE); // Large display
+char dl[16][16],dm[16][16],dr[16][16];
+//=================================================
+void NB_oledDraw() 
+//=================================================
+{
+ u8g.firstPage();  
+  do {
+        draw();
+  } while( u8g.nextPage() ); 
+}
+//=================================================
+void clearOled()
+//================================================= 
+{
+  int i;
+  for(i=1;i<=4;i++)
+  {
+    strcpy(dl[i]," ");
+    strcpy(dm[i]," ");
+    strcpy(dr[i]," ");
+  }
+}
+//=================================================
+void draw()
+//=================================================
+{
+  // Horizontal pixels: 0 - 120
+  // Vertical pixels: 0 - 63
+  u8g.setFont(u8g_font_6x10);
+  //u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  
+  u8g.drawStr( 0, 10, dl[1]);
+  u8g.drawStr( 0, 27, dl[2]);
+  u8g.drawStr( 0, 45, dl[3]);
+  u8g.drawStr( 0, 62, dl[4]);
+
+  u8g.drawStr( 45, 10, dm[1]);
+  u8g.drawStr( 99, 27, dm[2]);
+  u8g.drawStr( 99, 45, dm[3]);
+  u8g.drawStr( 45, 62, dm[4]);
+
+  u8g.drawStr( 100, 10, dr[1]);
+  u8g.drawStr( 100, 27, dr[2]);
+  u8g.drawStr( 100, 45, dr[3]);
+  u8g.drawStr( 100, 62, dr[4]);  
+
+}
 void setup() {
   int x,i;
 pinMode(8, OUTPUT);
 pinMode(9, OUTPUT);
 pinMode(10, OUTPUT);
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
   for(i=1;i<100;i++)
   {
@@ -13,22 +64,45 @@ pinMode(10, OUTPUT);
     delay(10);
     limit = x;
   }
-  Serial.print(limit); Serial.print(" ");
-  Serial.println(x);
+  //Serial.print(limit); Serial.print(" ");
+  //Serial.println(x);
+  
+  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+    u8g.setColorIndex(255);     // white
+  }
+  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+    u8g.setColorIndex(3);         // max intensity
+  }
+  else if ( u8g.getMode() == U8G_MODE_BW ) {
+    u8g.setColorIndex(1);         // pixel on
+  }
+  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+    u8g.setHiColorByRGB(255,255,255);
+  }
+  clearOled();
 }
 
-
+long t1,t2,dt;
+int sig = 0;
 void loop() {
 
   int x = analogRead(A0);
+  int delta = x - limit;
 
- if(x > limit + 1) 
+ if(x > limit + 8) 
  {
   digitalWrite(8,HIGH);
   digitalWrite(9,LOW);
   digitalWrite(10,LOW);
+  if(sig == 0)
+  {
+  t2 = t1;
+  t1 = millis();
+  dt = t1 - t2;
+  sig = 1;
+  }
  }
- else if(x < limit-1) 
+ else if(x < limit-8) 
  {
   digitalWrite(8,LOW);
   digitalWrite(9,LOW);
@@ -39,9 +113,14 @@ void loop() {
   digitalWrite(8,LOW);
   digitalWrite(9,HIGH);
   digitalWrite(10,LOW);
+  sig = 0;
  }
+  //Serial.print(x); Serial.print(" ");
   //Serial.print(limit); Serial.print(" ");
-  //Serial.println(x);
-  delay(10); 
-  limit = limit + (x-limit)*0.0001;
+  //Serial.println(delta);
+  sprintf(dl[4],"%3d",delta);
+    sprintf(dl[3],"%d",dt);
+  NB_oledDraw();
+  //delay(10); 
+  //limit = limit + (x-limit)*0.0001;
 }
