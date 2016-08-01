3649ab0c0233f258553d510d3f92858762bf892e
bsaxen <benny.saxen@gmail.com>
2016-08-01 13:24:25 +0200

    Update SW-18-hall-sensor.ino

--- sw/SW-18-hall-sensor.ino
+++ sw/SW-18-hall-sensor.ino
@@ -6,30 +6,8 @@ int g_app_id = 18;
 //==================================================
 #include <U8glib.h>
 
-int limit = 512;
 U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE); // Large display
 char dl[16][16],dm[16][16],dr[16][16];
-//=================================================
-void NB_oledDraw() 
-//=================================================
-{
- u8g.firstPage();  
-  do {
-        draw();
-  } while( u8g.nextPage() ); 
-}
-//=================================================
-void clearOled()
-//================================================= 
-{
-  int i;
-  for(i=1;i<=4;i++)
-  {
-    strcpy(dl[i]," ");
-    strcpy(dm[i]," ");
-    strcpy(dr[i]," ");
-  }
-}
 //=================================================
 void draw()
 //=================================================
@@ -57,103 +35,116 @@ void draw()
 
 }
 //=================================================
+void NB_oledDraw() 
+//=================================================
+{
+ u8g.firstPage();  
+  do {
+        draw();
+  } while( u8g.nextPage() ); 
+}
+//=================================================
+void clearOled()
+//================================================= 
+{
+  int i;
+  for(i=1;i<=4;i++)
+  {
+    strcpy(dl[i]," ");
+    strcpy(dm[i]," ");
+    strcpy(dr[i]," ");
+  }
+}
+
+//=================================================
 void setup() 
 //=================================================
 {
-  int x,i;
-pinMode(8, OUTPUT);
-pinMode(9, OUTPUT);
-pinMode(10, OUTPUT);
 
-  for(i=1;i<100;i++)
+
+  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) 
   {
-    x = analogRead(A0);
-    delay(10);
-    limit = x;
-  }
-  
-  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
     u8g.setColorIndex(255);     // white
   }
-  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) 
+  {
     u8g.setColorIndex(3);         // max intensity
   }
-  else if ( u8g.getMode() == U8G_MODE_BW ) {
+  else if ( u8g.getMode() == U8G_MODE_BW ) 
+  {
     u8g.setColorIndex(1);         // pixel on
   }
-  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) 
+  {
     u8g.setHiColorByRGB(255,255,255);
   }
+  
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+
   clearOled();
   strcpy(dl[1],"Volvo Amazon"); 
-  NB_oledDraw();
+  //NB_oledDraw();
 }
 
-long t1,t2;
-int dt;
+unsigned long t1,t2;
+unsigned long dt;
 int sig = 0;
-int counter = 0;
-int freq;
+unsigned long counter = 0;
 
-int xmin = 999;
-int xmax = 0;
-int ss;
+unsigned long xmin = 999;
+unsigned long xmax = 0;
+unsigned long ss;
+unsigned long rpm;
 int sensitivity = 5;
+unsigned long x,x1,x2,x3;
+int rs = 3;
 //=================================================
 void loop() 
 //=================================================
 {
   counter++;
-  int x = analogRead(A0);
-  //int delta = x - limit;
-  if(xmax < x)xmax = x;
-  if(xmin > x)xmin = x;
 
- if(x > xmax - sensitivity) 
+  delay(rs);
+  x = analogRead(A0);
+  x3 = x2;x2 = x1;x1 = x;
+
+
+if(x1 < (x2 - sensitivity) && x3 < (x2 - sensitivity)) // maximum found
  {
   digitalWrite(8,HIGH);
-  //digitalWrite(10,LOW);
-  if(sig == 0)
-  {
+  delay(rs);
   t2 = t1;
   t1 = millis();
   dt = t1 - t2;
-  sig = 1;
-  }
  }
- else if(x < xmin + sensitivity) 
- {
+
   digitalWrite(8,LOW);
-  //digitalWrite(10,HIGH);
-    sig = 0;
- }
+  digitalWrite(9,HIGH);
+  digitalWrite(9,LOW);
 
-
-  if(counter == 10000)
+  if(counter > 600/rs)
   {
-    //sprintf(dl[2],"min=%d",xmin);
-    //sprintf(dm[2],"max=%d",xmax);
-    ss = xmax - xmin;
-    if(ss <= 2*sensitivity)
-    {
-      strcpy(dl[2],"signal = no");
+      if(dt > 0)
+        rpm = 60*(1000/dt);
+      else
+        rpm = 0; 
+      sprintf(dl[2],"data=%5ld",x);
+      sprintf(dl[3],"rpm =%5ld",rpm);
+      sprintf(dl[4],"dt  =%5ld",dt);
+      digitalWrite(8,LOW);
       digitalWrite(10,HIGH);
-    }
-    else
-    {
-      sprintf(dl[2],"signal=%d",ss);
-      digitalWrite(10,LOW);
-    }
-    freq = 1000/dt;
-    sprintf(dl[3],"freq=%d",freq);
-    digitalWrite(8,LOW);
-    digitalWrite(9,HIGH);
     
     NB_oledDraw();
     counter = 0;
-    xmin = 999;
-    xmax = 0;
-    digitalWrite(9,LOW);
+    digitalWrite(10,LOW);
+    rpm = 0;
+    t1 = millis();
+    dt = 0;
   }
   
   
