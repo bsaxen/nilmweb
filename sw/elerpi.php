713a20605cbae5446e0de24e5f7367cd93f4bda7
bsaxen <benny.saxen@gmail.com>
2016-08-11 09:25:35 +0200

    Create elerpi.php

---
+++ sw/elerpi.php
@@ -0,0 +1,36 @@
+<?php
+//=======================================
+function logData($sid,$data)
+//=======================================
+{
+   $yymmdd = date('Y-m-d');
+   $hhmmss = date('H:i:s'); 
+   $filename = $sid.'-'.$yymmdd.'.elerpi';
+   $item = $hhmmss.' '.$data." \n";
+
+   if (file_exists($filename))
+   {
+      $fh = fopen($filename, "a");
+      fwrite($fh, $item);
+      fclose($fh);
+   }
+   else
+   {
+      $fh = fopen($filename, "w");
+      fwrite($fh, $item);
+      fclose($fh);
+      chmod($filename,0777);
+   }              
+}
+
+if(isset($_GET['sid']))
+{
+    $sid    = $_GET["sid"];
+    $name   = $_GET["name"];
+    $ip     = $_GET["ip"];
+    $data   = $_GET["dat"];
+    logData($sid,$data);
+}
+
+?>
+
