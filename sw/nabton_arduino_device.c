161e3f6bc56cdb829f96c097fb6526e3ed885557
bsaxen <benny.saxen@gmail.com>
2016-11-30 16:31:14 +0100

    Update nabton_arduino_device.c

--- sw/nabton_arduino_device.c
+++ sw/nabton_arduino_device.c
@@ -1,1 +1,28 @@
-asdasd
+void nb_send()
+{
+}
+
+void nb_rec()
+{
+  
+}
+
+
+
+
+setup()
+{
+  // Connect to Nabton Device Server
+  
+  
+  // receive pinMode settings
+ 
+  
+  
+}
+
+loop()
+{
+  // Send status of all digitalpins
+  // analog values for all analog inputs
+}
