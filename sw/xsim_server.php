93333b043f055ac9dbe165ec9fbe55a2ac87b9ec
bsaxen <benny.saxen@gmail.com>
2016-12-07 12:57:12 +0100

    Update xsim_server.php

--- sw/xsim_server.php
+++ sw/xsim_server.php
@@ -3,12 +3,11 @@
 // server-houseRpi.order
 
 $x_from = $_GET['from']; // From client Id
-$x_to   = $_GET['to']; // To client Id
 $x_msg  = $_GET['msg']; // Transaction Message type: 1 = any orders for me?, 2 = answer on latest order
 
 function saveAnswer($from,$to,$answer)
 {
-  $file = $from.'-'.$to.'.response';
+  $file = $from.'.response';
   $fh = fopen($file, 'w') or die("saveAnswer can't open file: $file");
   fwrite($fh, $answer);    
   //fwrite($fh, "\n");
@@ -17,7 +16,7 @@ function saveAnswer($from,$to,$answer)
 function getNextTransaction($id)
 {
   $file = $id.'.order';
-  $fh = fopen($file, 'r') or die("getNextTransaction can't open file: $file");
+  $fh = fopen($file, 'r') or die("empty");
   $row = fgets($fh);
   echo("$row");                       
   fclose($fh);
@@ -26,9 +25,9 @@ function getNextTransaction($id)
 
 if ($x_msg == 2)
 {
-  $x_response = $_GET['x_answer'];
+  $x_result = $_GET['result'];
   //echo("<br>From:$x_from To:$x_to Answer:$x_answer");
-  saveAnswer($x_from,$x_to,$x_answer);
+  saveAnswer($x_from,$x_result);
 }
 else if ($x_msg == 1)
 {
